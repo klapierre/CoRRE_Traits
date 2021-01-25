0c50e9bf8779a330f2384f3297fc41d4ef2d4386
kaitkimmel <kimme099@umn.edu>
2021-01-25 14:13:12 -0600

    Start of HPGRS_PHACE code. still need to figure out how to deal with treatment year.

---
+++ DatasetCleaningCode/HPGRS_PHACE.R
@@ -0,0 +1,34 @@
+#######################
+#### HPGSRS_PHACE #####
+#####################
+
+setwd("/Users/kaitlinkimmel/Dropbox/CoRRE_database")
+
+# library
+library(readxl)
+library(tidyr)
+
+# data
+dat <- read_excel("Data/OriginalData/2020 update/Data/HPGRS_PHACE_data.xlsx")
+
+dat$treatment_year <- dat$YEAR - 2005
+# How to add in warming treatment year?
+# color?
+dat$site_code <- "HPGRS"
+dat$project_name <- "PHACE"
+dat$data_type <- "biomass"
+
+dat <- dat[-c(3,4,6,7,8,9)] # get rid of unnecessary columns
+names(dat)[c(1:5)] <- c("calendar_year", "plot_id", "block", "treatment", "anpp") #rename columms
+
+bio_dat <- dat[,-c(6:59)] #pull out composite anpp data
+
+write.csv(bio_dat, "Data/CleanedData/Sites/ANPP csv/HPGRS_PHACE_anpp.csv", row.names = FALSE) #save anpp data
+
+dat <- dat[,-5]
+dat <- gather(dat, key = "genus_species", value = "abundance", 5:58) # wide to long format
+dat <- dat[which(dat$abundance > 0),] # get rid of species with 0 biomass
+
+write.csv(dat, "Data/CleanedData/Sites/Species csv/HPGRS_PHACE.csv", row.names = FALSE)
+
+
