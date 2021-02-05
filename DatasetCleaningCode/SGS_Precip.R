e4a9fa3632811f8b47adb149d8c557605d883dae
kaitkimmel <kimme099@umn.edu>
2021-02-05 12:25:23 -0600

    Updated NutNet and SGS code

--- DatasetCleaningCode/SGS_Precip.R
+++ DatasetCleaningCode/SGS_Precip.R
@@ -9,10 +9,12 @@ library(readxl)
 library(tidyr)
 
 # read in data
-dat <- read_excel("Data/OriginalData/2020 update/Data/SGS_Precip.xlsx")
+dat <- read_excel("Data/OriginalData/2020 update/Data/SGS/SGS_Precip.xlsx")
+sp.list <- read_excel("Data/OriginalData/2020 update/Data/SGS/plant_list-SGS.xls")
 
+dat <- dat[,-53]
 # Long to wide format
-dat <- gather(dat, key = "sp_code", value = "abundance", 4:length(dat))
+dat <- gather(dat, key = "sp_code", value = "abundance", 4:52)
 
 #Code in treatments
 dat$TREAT[dat$TREAT == 1] <- "reduction"
@@ -36,3 +38,52 @@ dat$project_name <- "Precip"
 dat$data_type <- "cover"
 
 names(dat)[c(1:3)] <- c("calendar_year", "plot_id", "treatment")
+
+# Species data provided by Kerry - used 6 letters for sp_code, 
+# but does not know exactly how they line up with the USDA Plants database which only uses 4
+# Gave me a partial list of species found at SGS site, so am using that to match to the species 
+# codes in the dataset
+# get column needed
+sp.list <- sp.list[,2]
+# Add column name
+names(sp.list)<- "genus_species"
+sp.list <- sp.list[-which(is.na(sp.list$genus_species)),]
+# split genus and species into separate columns
+temp <- strsplit(sp.list$genus_species, " ")
+# add columns to df
+sp.list$genus <- sapply(temp, "[[", 1)
+sp.list$species <- sapply(temp, "[[", 2)
+#pull out first 3 letters of genus
+sp.list$genus3 <- substr(sp.list$genus, 1, 3)
+# pull out first 3 letters of sp
+sp.list$sp3 <- substr(sp.list$species, 1, 3)
+# combine genus and species 3 letters to get code
+sp.list$sp_code <- paste(sp.list$genus3, sp.list$sp3, sep = "")
+# make sp_code all uppercase like in dataframe
+sp.list$sp_code <- toupper(sp.list$sp_code)
+
+
+# get unique species from dataframe
+sp.dat <- data.frame(sp_code = unique(dat$sp_code))
+sp.match <- merge(sp.dat, sp.list[,c(1,6)], all.x= TRUE)
+unmatched <- sp.match[which(is.na(sp.match$genus_species)),]
+
+# unmatched species
+# ALITEX = Allium textile
+# ARIPUR = ??
+# CHR = Chrysothamnus viscidiflorus
+# SPURGE = Unknown spurge
+# UNK = Unknown
+
+unmatched$genus_species <- c("Allium textile", "??", "Chrysothamnus viscidiflorus", "Unknown spurge", "Unknown")
+sp.list <-sp.list[,c(1,6)]
+sp.list <- rbind(sp.list, unmatched)
+sp.match <- merge(sp.list, sp.dat, all.y = TRUE)
+
+dat <- merge(dat, sp.match)
+
+dat <- dat[,-1]
+
+write.csv(dat, "Data/CleanedData/Sites/Species csv/SGS_Precip.csv", row.names = FALSE)
+
+
