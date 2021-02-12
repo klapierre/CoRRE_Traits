ef711bc4c3aebf9fbaff8ceb517f23fe89801d1e
kaitkimmel <kimme099@umn.edu>
2021-02-12 12:42:02 -0600

    adding tree or not code

--- DatasetCleaningCode/Species_Names2020.R
+++ DatasetCleaningCode/Species_Names2020.R
@@ -21,7 +21,7 @@ library(Taxonstand)
 full.list <- read.csv ("Data/CompiledData/Species_lists/SpeciesList.csv", row.names = 1)
 oldsp <- read.csv("Data/CompiledData/CoRRE_TRY_species_list.csv", row.names = 1)
 moss <- read.delim("Data/OriginalData/Traits/Bryophytes.txt", sep = ",", header = FALSE) #from: http://www.mobot.org/mobot/tropicos/most/bryolist.shtml
-
+tree.spold <- read.csv("Data/TRYCoRREMerge/species_families_trees_compelete.csv")
 # Manipulating oldsp to prepare to merge with the new species list
 oldsp$oldsp <- 1 # create a column to indicate name was in old species list
 oldsp1 <- oldsp[,c(1,4)] # get rid of extra columns
@@ -80,13 +80,13 @@ unmatched.sp$species_matched <- gsub("lychnis latifolia", "Silene latifolia", un
 unmatched.sp$species_matched <- gsub("oxytropis caerulea", "Oxytropis coerulea", unmatched.sp$species_matched)
 unmatched.sp$species_matched <- gsub("petalostemum villosum", "Dalea villosa", unmatched.sp$species_matched)
 unmatched.sp$species_matched[unmatched.sp$species_matched == "plantago (purshii)"] <- "Plantago patagonica"
-unmatched.sp$species_matched <- gsub("ranoncolos rhombiodeu", "Ranunculus rhomboideus", unmatched.sp$species_matched)
+unmatched.sp$species_matched <- gsub("ranoncolos rhombiodeus", "Ranunculus rhomboideus", unmatched.sp$species_matched)
 unmatched.sp$species_matched <- gsub("robinia pseudo acacia", "Robinia pseudoacacia", unmatched.sp$species_matched)
 unmatched.sp$species_matched <- gsub("rubus pennsylvanicus", "Rubus pensilvanicus", unmatched.sp$species_matched)
 unmatched.sp$species_matched <- gsub("silene jenisseensis", "Silene jenisseensis", unmatched.sp$species_matched)
 unmatched.sp$species_matched <- gsub("symphyotrichum novae", "Symphyotrichum novae-angliae", unmatched.sp$species_matched)
 unmatched.sp$species_matched[unmatched.sp$species_matched == "pycnan vir"] <- "Pycnanthemum virginianum"
-unmatched.sp$species_matched[unmatched.sp$species_matched == "solanum grascilens bonariensis blanco"] <- "Solanum chenopodioides"
+unmatched.sp$species_matched[unmatched.sp$species_matched == "solanum grascilens"] <- "Solanum chenopodioides"
 unmatched.sp$species_matched[unmatched.sp$species_matched == "marsilea cocinea"] <- "Marsilea ancylopoda"
 unmatched.sp$species_matched[unmatched.sp$species_matched == "gliseria peruviana"] <- "Glyceria multiflora"
 unmatched.sp$species_matched[unmatched.sp$species_matched == "unkown big leaf"] <- "Unknown"
@@ -103,9 +103,17 @@ unmatched.sp$species_matched[grepl("un ",fixed = TRUE, unmatched.sp$species_matc
 
 # change column names
 names(unmatched.sp)[1] <- "genus_species"
+
+## Run unmatched thru TPL again
+
+unmatched.sp.new <- TPL(unmatched.sp$species_matched)
+unmatched.sp.new$species_matched <- paste(unmatched.sp.new$New.Genus, unmatched.sp.new$New.Species, sep = " ")
+corrected.names <- unmatched.sp.new[which(unmatched.sp.new$New.Species != "sp"),]
+corrected.names <- corrected.names[,c(1,27,13,26)]
+names(corrected.names) <- c("genus_species","species_matched","Family","Tax_res")
 # combine with matched species
 matched.sp <- read.csv("Data/CompiledData/Species_lists/good_sp.csv", row.names = 1)
-newsp.match <- rbind(matched.sp, unmatched.sp)
+newsp.match <- rbind(matched.sp, corrected.names)
 
 
 unique.newsp <- data.frame(species_matched = (unique(newsp.match$species_matched)), new.sp = 1)
@@ -118,9 +126,45 @@ need.traits <- all.sp[which(all.sp$new.sp ==1 & all.sp$old.sp ==0),]
 need.traits <- need.traits[!grepl(" sp",fixed = TRUE, need.traits$species_matched),]
 need.traits <- need.traits[!grepl("Unknown",fixed = TRUE, need.traits$species_matched),]
 need.traits <- merge(need.traits, unique(newsp.match[,c(2,3)]), all.x = TRUE)
+need.traits <- need.traits[-which(need.traits$species_matched == "pioneer mosses"),]
+
+# caplitalize first letter of species names function modified from https://rstudio-pubs-static.s3.amazonaws.com/408658_512da947714740b99253228f084a08a9.html
+CapStr <- function(y) { 
+  c <- strsplit(y, " ")[[1]][1]
+  d <- strsplit(y, " ")[[1]][2]
+  paste(paste(toupper(substring(c, 1,1)), substring(c, 2),
+              sep="", collapse=" "), d, sep = " ")
+}
+need.traits$species_matched <- sapply(need.traits$species_matched, CapStr)
+
+moss$Family <- sapply(strsplit(moss$V1, split = " "),`[`, 1, simplify=FALSE)
+moss <- moss[which(moss$Family != ""),]
+need.traits <- need.traits[-which(need.traits$Family %in% moss$Family),]
+need.traits <- need.traits[-which(need.traits$Family == "Anastrophyllaceae"),]
+need.traits <- need.traits[-which(need.traits$species_matched == "Nephroma arcticum"),]
+
+need.traits$Family[need.traits$species_matched == "Dichelachne squamulosum"] <- "Poaceae"
+need.traits$Family[need.traits$species_matched == "Lespedeza discola"] <- "Fabaceae"
+need.traits$Family[need.traits$species_matched == "Robinia pseudo"] <- "Fabaceae"
+need.traits$Family[need.traits$species_matched == "Silene jenisseensis"] <- "Caryophyllaceae"
 
 write.csv(need.traits, "Data/CompiledData/Species_lists/newsp2020.csv")
 
+
+### Create file to indicate if the species is a tree or not
+tree.sp <- need.traits
+tree.sp$tree.non.tree <- "non-tree"
+tree.sp$tree.non.tree[tree.sp$species_matched %in% c("Asimina triloba", "Juniperus communis", "Diospyros virginiana", 
+                                       "Tilia europaea", "Morus rubra", "Fraxinus excelsior", "Crataegus secta",
+                                       "Prunus avium", "Prunus padus","Pyrus pyraster", "Populus tremula",
+                                       "Salix atrocinerea", "	Acer pseudoplatanus")| 
+          tree.sp$Family %in% c("Betulaceae", "Fagaceae", "Juglandaceae", "Pinaceae")] <- "tree"
+
+# merge with previous version
+tree.sp <- tree.sp[,c(1,4,5)]
+names(tree.sp)[2]<- "family"
+tree.spnew <- rbind(tree.sp, tree.spold)
+write.csv(tree.spnew,"Data/TRYCoRREMerge/species_families_trees_compelete_2020.csv", row.names = FALSE)
 
 #### Create a dataframe with full species list by combining old sp with new sp. 
 names(newsp.match)[4] <- "type"
@@ -153,6 +197,7 @@ full.splist$remove <- 0
 full.splist$remove[full.splist$species_matched == "Unknown NA"] <- 3
 full.splist$remove[full.splist$type == "identified genus"] <- 1
 full.splist$remove[full.splist$type == "moss/lichen"] <- 2
+full.splist$remove[full.splist$species_matched == "Pioneer mosses"] <- 2
 
 write.csv(full.splist, "Data/CompiledData/Species_lists/fullsp_list2020.csv", row.names = FALSE)
 ### Notes: lespedeza discola???
