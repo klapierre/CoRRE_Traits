8f413785fdca2f94c8199bbd9059e4a78a47bfb0
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-03-06 17:28:53 -0500

    Cleaning code.

--- Traits/merge_all_trait_databases.R
+++ Traits/merge_all_trait_databases.R
@@ -1,40 +1,47 @@
+################################################################################
+##  merge_all_trait_databases.R: Putting together data from TRY, BIEN, and AusTraits.
+##
+##  Authors: Kimberly Komatsu, Meghan Avolio
+################################################################################
+
 library(tidyverse)
 
 setwd('C:\\Users\\mavolio2\\Dropbox\\CoRRE_database\\Data') #meghan's
 setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data') #kim's
 
-#read in names
+
+
+#### Read in data ####
+
+# Cleaned species names
 names <- read.csv("CompiledData\\Species_lists\\species_families_trees_2021.csv")
 
-
-#read in AusTraits
-AusTraits <- read.csv('OriginalData\\Traits\\AusTraits_2022\\AusTraits_CoRRE_Feb2023.csv')%>%
+# AusTraits
+AusTraits <- read.csv('OriginalData\\Traits\\AusTraits_2022\\AusTraits_CoRRE_March2023.csv')%>%
   left_join(names)%>%
   filter(tree.non.tree=="non-tree") %>% 
   mutate(species_matched2=species_matched)%>%
   separate(species_matched2, into=c('genus', 'species'))%>%
   mutate(DatabaseID='AusTraits')%>%
   select(DatabaseID, DatasetID, ObservationID, family, species_matched, genus, CleanTraitName, StdValue)
-  
 
 
-
-#read in TRY
-TRY <- read.csv('OriginalData\\Traits\\TRY\\TRY_trait_data_continuous_long_Nov2022.csv') %>% 
+# TRY
+TRY <- read.csv('OriginalData\\Traits\\TRY\\TRYCoRREMerge\\TRY_trait_data_continuous_long_March2023.csv') %>% 
   mutate(DatabaseID="TRY")
 
-#read in BIEN
-BIEN<-read.csv('OriginalData\\Traits\\BIEN\\BIEN_trait_data_continuous_Nov2022.csv')
+# BIEN
+BIEN <- read.csv('OriginalData\\Traits\\BIEN\\BIEN_trait_data_continuous_Nov2022.csv')
 
-#rbind all together
+# Bind all together
 allTraits <- rbind(TRY, AusTraits, BIEN) %>% 
   select(DatabaseID, DatasetID, ObservationID, family, genus, species_matched, CleanTraitName, StdValue)
 
-# How well correlated is BIEN SLA with the others?
-# test <- allTraits %>% 
-#   group_by(DatabaseID, species_matched, CleanTraitName) %>% 
-#   summarise(mean=mean(StdValue)) %>% 
-#   ungroup() %>% 
+# How well correlated is BIEN SLA with the others? Pretty good, despite not knowing whether the data was collected with or without petiole or on leaves or leaflets when leaves are compound.
+# test <- allTraits %>%
+#   group_by(DatabaseID, species_matched, CleanTraitName) %>%
+#   summarise(mean=mean(StdValue)) %>%
+#   ungroup() %>%
 #   pivot_wider(names_from=DatabaseID, values_from=mean)
 # 
 # ggplot(data=subset(test, !is.na(TRY) & !is.na(BIEN) & CleanTraitName=='SLA'), aes(x=TRY, y=BIEN)) +
@@ -47,27 +54,22 @@ allTraits <- rbind(TRY, AusTraits, BIEN) %>%
 # cor.test(test2$TRY, test2$AusTraits)
 
 
-#transpose data like Franzi wants
-talltraits<-allTraits %>% 
-  group_by(DatabaseID, DatasetID, ObservationID, family, genus, species_matched)%>%
-  spread(CleanTraitName, StdValue, fill=NA)
+# Transpose to wide format for gap filling.
+talltraits <- allTraits %>% 
+  group_by(DatabaseID, DatasetID, ObservationID, family, genus, species_matched) %>%
+  pivot_wider(names_from=CleanTraitName, values_from=StdValue, values_fill=NA) %>% 
+  ungroup()
 
-# write.csv(allTraits, 'OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_Nov2022_long.csv', row.names = F)
+# write.csv(allTraits, 'OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023_long.csv', row.names = F)
 
-# write.csv(talltraits, 'OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_Nov2022.csv', row.names = F)
+# write.csv(talltraits, 'OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023.csv', row.names = F)
 
 ##checking traits
-test<-allTraits %>% 
-  group_by(DatabaseID, species_matched, CleanTraitName, StdValue) %>% 
+test <- allTraits %>% 
+  group_by(species_matched, CleanTraitName, StdValue, DatabaseID) %>% 
   summarize(n=length(StdValue)) %>% 
-  filter(n>10)
+  ungroup() %>% 
+  filter(n>9)
 
-##all databases have repeats - 53488
-##only 1009 are the same values repeated 10 or more times when controlling for database
-
-test2<-allTraits %>% 
-  group_by(species_matched, CleanTraitName, StdValue) %>% 
-  summarize(n=length(StdValue)) %>% 
-  filter(n>10)
-
-#1154 values are repeated 10 or more times across all data
+## all databases have repeats - 72,936 across all data
+## only 5597 are the same values repeated 10 or more times and were designated as keepers from cleaning code
