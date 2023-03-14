f88a192e1198ed6b67db61d64a404f227985ce6d
Kimberly Komatsu <KJKOMATSU@uncg.edu>
2023-03-14 15:01:59 -0400

    Initial outlier checks and merging data.

--- Traits/07_finalizing continuous and catagorical trait data.R
+++ Traits/07_finalizing continuous and catagorical trait data.R
@@ -14,6 +14,12 @@ library(tidyverse)
 setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data') #Kim's
 setwd("C:\\Users\\wilco\\Dropbox\\shared working groups\\sDiv_sCoRRE_shared\\CoRRE data\\") # Kevin's laptop wd
 
+theme_set(theme_bw())
+theme_update(axis.title.x=element_text(size=20, vjust=-0.35, margin=margin(t=15)), axis.text.x=element_text(size=16),
+             axis.title.y=element_text(size=20, angle=90, vjust=0.5, margin=margin(r=15)), axis.text.y=element_text(size=16),
+             plot.title = element_text(size=24, vjust=2),
+             panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+             legend.title=element_blank(), legend.text=element_text(size=20))
 
 
 #### Categorical trait data ####
@@ -21,7 +27,7 @@ setwd("C:\\Users\\wilco\\Dropbox\\shared working groups\\sDiv_sCoRRE_shared\\CoR
 # The rest were not complete (dispersal mode, pollinaton syndrome).
 
 catagoricalTraits <- read.csv("CleanedData\\Traits\\complete categorical traits\\sCoRRE categorical trait data_12142022.csv") %>%
-  dplyr::select(species_matched, growth_form, photosynthetic_pathway, lifespan,  clonal, mycorrhizal_type, n_fixation) %>%
+  dplyr::select(family, species_matched, growth_form, photosynthetic_pathway, lifespan,  clonal, mycorrhizal_type, n_fixation) %>%
   mutate(photosynthetic_pathway = replace(photosynthetic_pathway, grep("possible", photosynthetic_pathway), NA)) %>%
   mutate(clonal = replace(clonal, clonal=="uncertain", NA)) %>%
   mutate(mycorrhizal_type = replace(mycorrhizal_type, mycorrhizal_type=="uncertain", NA)) %>%
@@ -32,17 +38,25 @@ catagoricalTraits <- read.csv("CleanedData\\Traits\\complete categorical traits\
 #### Continuous traits ####
 # NOTE: Continuous traits to include: LDMC, SLA, Vegetative_height, seed dry mass, seed number, rooting density, rooting depth. 
 
+# Read species data to remove mosses
+mossKey <- read.csv("CleanedData\\Traits\\complete categorical traits\\sCoRRE categorical trait data_12142022.csv") %>%
+  dplyr::select(species_matched, leaf_type) %>%
+  mutate(moss = ifelse(leaf_type=="moss", "moss","non-moss")) %>%
+  dplyr::select(-leaf_type)
+
 # Read in imputed trait data and bind on species information
 imputedRaw <- read.csv("CleanedData\\Traits\\gap filled continuous traits\\imputed_traits_mice.csv") %>%
   dplyr::select(-X) %>% 
-  bind_cols(read.csv('OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023.csv')[,c('DatabaseID', 'DatasetID', 'ObservationID', 'family', 'genus', 'species_matched')])
+  bind_cols(read.csv('OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023b.csv')[,c('DatabaseID', 'DatasetID', 'ObservationID', 'family', 'genus', 'species_matched')]) %>%   
+  left_join(mossKey) %>% 
+  filter(moss!="moss") %>%
+  dplyr::select(-moss) #removes 556 species observations
 
 imputedLong <- imputedRaw %>% 
   pivot_longer(names_to='trait', values_to='imputed_value', seed_dry_mass:X58)
 
-
 # Read original trait data and join with imputed data
-originalRaw <- read.csv('OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023.csv') %>%
+originalRaw <- read.csv('OriginalData\\Traits\\raw traits for gap filling\\TRYAusBIEN_continuous_March2023b.csv') %>%
   pivot_longer(names_to='trait', values_to='original_value', seed_dry_mass:X58) %>%
   na.omit()
 
@@ -57,23 +71,40 @@ allContinuous <- imputedLong %>%
                       'rooting_depth', 'seed_dry_mass', 'seed_length', 'seed_number', 'seed_terminal_velocity', 'SLA',
                       'SRL', 'stem_spec_density', 'stomatal_conductance')) # dropped J_max and Vc_max because they directly relate to photosynthesis
 
-
 # Calculate averages for each species
 meanContinuous <- allContinuous %>% 
-  group_by(species_matched, trait) %>% 
+  group_by(family, species_matched, trait) %>% 
   summarize_at(.vars=c('imputed_value', 'original_value'),
                .funs=list(mean=mean, sd=sd),
                na.rm=T) %>% 
   ungroup()
 
+speciesCount <- meanContinuous %>% 
+  group_by(family) %>% 
+  summarize(num_species=length(family)) %>% 
+  ungroup()
+
 
 # Compare imputed to original continuous trait data
-ggplot(data=na.omit(allContinuous), aes(x=original_value, y=imputed_value)) +
+ggplot(data=na.omit(meanContinuous), aes(x=original_value_mean, y=imputed_value_mean)) +
   geom_point() +
   geom_abline(slope=1) +
   facet_wrap(~trait, scales='free')
 
-ggplot(data=na.omit(meanContinuous), aes(x=original_value_mean, y=imputed_value_mean)) +
+# Only grasses -- 11620 species
+ggplot(data=na.omit(subset(meanContinuous, family=='Poaceae')), aes(x=original_value_mean, y=imputed_value_mean)) +
+  geom_point() +
+  geom_abline(slope=1) +
+  facet_wrap(~trait, scales='free')
+
+# Only asters -- 11480
+ggplot(data=na.omit(subset(meanContinuous, family=='Asteraceae')), aes(x=original_value_mean, y=imputed_value_mean)) +
+  geom_point() +
+  geom_abline(slope=1) +
+  facet_wrap(~trait, scales='free')
+
+# Only legumes -- 4620
+ggplot(data=na.omit(subset(meanContinuous, family=='Fabaceae')), aes(x=original_value_mean, y=imputed_value_mean)) +
   geom_point() +
   geom_abline(slope=1) +
   facet_wrap(~trait, scales='free')
@@ -88,12 +119,13 @@ ggplot(data=na.omit(allContinuous), aes(x=trait, y=imputed_value)) +
 #### Clean imputed continuous trait data ####
 ####Check these decisions with Meghan!
 
+#note: problematic trait is photosynthesis_rate. Things that look problematic but Kim thinks are real: leaf_area (some ferns and palms with huge leaves), plant_height_vegetative (vines that have big big heights like Vitus sp and virginia creeper), seed number (consistently high numbers for some species that probably do have lots of seeds)
+
 cleanContinuous <- allContinuous %>% 
   #filtering out negative values for everything except leaf transpiration rate (where photosynthesis rate is negative, should actually be 0)
   mutate(drop=ifelse(trait!='leaf_transp_rate' & imputed_value<0, 1, 0)) %>% 
-  filter(drop==0) %>% #drops 44695 observations
-  #RGR and seed terminal velocity look great! shouldn't drop any of their values
-  
+  filter(drop==0) %>% #drops 11822 observations
+  select(-drop)
 
 
 # Look at boxplots for each trait
@@ -103,67 +135,22 @@ ggplot(data=na.omit(cleanContinuous), aes(x=trait, y=imputed_value)) +
 
 
 ##### TO DO #####
-# how were outliers removed before gap filling -- should maybe do it species by species, because there are some outliers for andro and achillea that might not be outliers in the entire dataset
-# figure out how to merge the sd with the imputed backtransformed data
-# determine how to do the sd cutoffs (we cannot just do quantiles because then the same amount of data is dropped from each trait, which is unfair to the good traits and generous to the bad traits)
-# drop the outliers based on sd cutoffs and the extreme values
-# correlations for each trait with imputed data (see code below)
+# doesn't seem like there are any big outliers for each trait overall, but when comparing individual species means there are some
 # look up some values for species that we know and make sure they are right
 
+ggplot(data=subset(meanContinuous, species_matched %in% c('Ruellia humilis', 'Andropogon gerardii', 'Parthenocissus quinquefolia')),
+       aes(x=species_matched, y=imputed_value_mean)) +
+  geom_boxplot() +
+  theme(axis.text.x=element_text(angle=40, vjust=0.5, size=3)) +
+  facet_wrap(~trait, scales='free')
 
 
+##### Combine continuous and categorical traits #####
+wideContinuous <- meanContinuous %>% 
+  select(-family, -original_value_mean, -imputed_value_sd, -original_value_sd) %>% 
+  pivot_wider(names_from=trait, values_from=imputed_value_mean, values_fill=NA)
 
-
-
-
-
-
-
-
-
-
-
-
-
-# hist(imputedSubset$LDMC_mean)
-# hist(imputedSubset$SLA_mean)
-# hist(imputedSubset$plant_height_vegetative_mean)
-# hist(imputedSubset$seed_dry_mass_mean)
-# hist(imputedSubset$seed_number_mean)
-# hist(imputedSubset$rooting_depth_mean)
-
-rm(imputedRaw)
-
-
-
-
-#### Removing mosses ####
-mossKey <- read.csv("complete categorical traits\\sCoRRE categorical trait data_12142022.csv") %>%
-  dplyr::select(species_matched, leaf_type) %>%
-  mutate(moss = ifelse(leaf_type=="moss", "moss","non-moss")) %>%
-  dplyr::select(-leaf_type)
-
-imputedSubset <- imputedRaw %>%
-  left_join(mossKey, by="species_matched") %>%
-  mutate(moss=ifelse(moss=="moss","moss","non-moss")) %>%
-  filter(moss!="moss") %>%
-  dplyr::select(-moss) #%>%
-# group_by(genus, family, species_matched) %>%
-# summarize_at(vars(seed_dry_mass:seed_number), list(mean=mean, sd=sd), na.rm=T) %>%
-# ungroup() #total of 1786 species remain
-
-# #total species count = 2403 (so 617 species dropped [likely because they had zero trait data and therefore can't have any data imputed])
-# sppList <- mossKey %>%
-#   filter(moss=='non-moss')
-
-
-
-###########################################################
-### Combine continuous and catagorical traits
-###########################################################
-
-traits_all <- dplyr::select(imputedSubset, -LDMC_sd:-rooting_depth_sd) %>%
+traitsAll <- wideContinuous %>%
   full_join(catagoricalTraits, by="species_matched")
 
-# Find species that have continuous data but no categorical data -- 3 of these are mosses, so I will remove them from continuous data in that cleaning script
-# TO DO: We will want to populate categorical data for three species: "Galium mollugo" "Heracleum sphondylium" "Trachypogon spicatus"
+# write.csv(traitsAll, 'CleanedData\\Traits\\CoRRE_allTraitData_March2023.csv')
