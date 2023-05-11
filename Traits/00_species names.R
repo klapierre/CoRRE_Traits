b97dc1790e6afcde0d8c0a9e6474114011876356
klapierre <kjkomatsu@uncg.edu>
2023-05-11 15:56:52 -0400

    Cleaning species names for GEx.

--- Traits/00_species names.R
+++ Traits/00_species names.R
@@ -8,27 +8,28 @@ library(tidyverse)
 
 setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data')
 
-gex <- read.csv('OriginalData\\Traits\\GEx_species_family.csv') %>% 
+gexOriginal <- read.csv('OriginalData\\Traits\\GEx_species_family.csv') %>% 
   rename(species_matched=clean_ejf,
          family=family_ejf) %>% 
+  select(family, species_matched, genus_species)
+
+gex <- read.csv('OriginalData\\Traits\\GEx_species_tree_complete.csv') %>% 
   mutate(database='GEx') %>% 
+  left_join(gexOriginal) %>% 
+  filter(tree.non.tree=='non.tree') %>% 
   select(database, family, genus_species, species_matched)
   
-
 corre <- read.csv('CompiledData\\Species_lists\\FullList_Nov2021.csv') %>% 
   select(genus_species, species_matched) %>% 
   full_join(read.csv('CompiledData\\Species_lists\\species_families_trees_2021.csv')) %>% 
   mutate(database='CoRRE') %>% 
   select(database, family, genus_species, species_matched)
 
-allNames <- rbind(corre, gex)
+allNames <- rbind(corre, gex) %>% 
+  na.omit() %>% 
+  separate(species_matched, into=c('genus', 'species', 'subspp'), sep=' ') %>% 
+  filter(!is.na(species)) %>% 
+  unite(col='species_matched', genus:species, sep=' ', remove=T) %>% 
+  select(-subspp)
 
-# write.csv(allNames, 'OriginalData\\Traits\\CoRRE_GEx_species_family_April2023.csv', row.names=F)
-
-tree <- read.csv('C:\\Users\\kjkomatsu\\Desktop\\GEx_family_list.csv') %>% 
-  left_join(allNames) %>% 
-  select(-genus_species) %>% 
-  filter(database=='GEx') %>% 
-  unique()
-
-# write.csv(tree, 'OriginalData\\Traits\\GEx_tree_April2023.csv', row.names=F)
+# write.csv(allNames, 'OriginalData\\Traits\\CoRRE_GEx_species_family_May2023.csv', row.names=F)
