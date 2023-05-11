a4b0d33fe4aee86b8fb7b2f48e6637d127f77b91
klapierre <kjkomatsu@uncg.edu>
2023-05-11 16:12:20 -0400

    Now GEx only.

--- Traits/00_species names.R
+++ Traits/00_species names.R
@@ -17,19 +17,11 @@ gex <- read.csv('OriginalData\\Traits\\GEx_species_tree_complete.csv') %>%
   mutate(database='GEx') %>% 
   left_join(gexOriginal) %>% 
   filter(tree.non.tree=='non.tree') %>% 
-  select(database, family, genus_species, species_matched)
-  
-corre <- read.csv('CompiledData\\Species_lists\\FullList_Nov2021.csv') %>% 
-  select(genus_species, species_matched) %>% 
-  full_join(read.csv('CompiledData\\Species_lists\\species_families_trees_2021.csv')) %>% 
-  mutate(database='CoRRE') %>% 
-  select(database, family, genus_species, species_matched)
-
-allNames <- rbind(corre, gex) %>% 
+  select(database, family, genus_species, species_matched) %>% 
   na.omit() %>% 
   separate(species_matched, into=c('genus', 'species', 'subspp'), sep=' ') %>% 
   filter(!is.na(species)) %>% 
   unite(col='species_matched', genus:species, sep=' ', remove=T) %>% 
   select(-subspp)
 
-# write.csv(allNames, 'OriginalData\\Traits\\CoRRE_GEx_species_family_May2023.csv', row.names=F)
+# write.csv(gex, 'OriginalData\\Traits\\GEx_species_family_May2023.csv', row.names=F)
