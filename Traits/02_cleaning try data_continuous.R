503fccec6788cec0df27aab2c8bba1aba8c97606
Kim Komatsu (She/Her/Hers) <KJKOMATSU@uncg.edu>
2023-10-18 11:26:21 -0400

    Putting duplicate dropping at the end together with problem datasets. Cleaning up code.

--- Traits/02_cleaning try data_continuous.R
+++ Traits/02_cleaning try data_continuous.R
@@ -44,7 +44,7 @@ corre_key <- read.csv("OriginalData\\Traits\\TRY\\corre2trykey_2021.csv") %>%
   select(species_matched, AccSpeciesID, AccSpeciesName) %>%
   unique()
 
-corresp<-corre_key %>% 
+corresp <- corre_key %>% 
   select(species_matched) %>% 
   unique() %>% 
   mutate(corre=1)
@@ -88,7 +88,141 @@ dat3 <- dat2 %>%
                         14, #leaf N
                         1080, 614, #SRL
                         26)) %>% #seed dry mass
-    mutate(remove=ifelse(TraitID==3116 & UnitName=='', 1, 
+  # give names to numbers for the core traits
+  mutate(CleanTraitName=ifelse(TraitID==14, 'leaf_N',
+                        ifelse(TraitID==26, 'seed_dry_mass', 
+                        ifelse(TraitID==47, 'LDMC', 
+                        ifelse(TraitID==55, 'leaf_dry_mass', 
+                        ifelse(TraitID==1080, 'SRL',
+                        ifelse(TraitID==3106, 'plant_height_vegetative', 
+                        ifelse(TraitID==3116, 'SLA', 
+                        ifelse(TraitID==3110, 'leaf_area',
+                        TraitID))))))))) %>%
+  filter(!is.na(StdValue)) %>% # drop observations without a trait value
+  filter(is.na(OrigObsDataID)) %>% # drop known repeats in TRY 
+  filter(UncertaintyName!="Range" & UncertaintyName!="Class range") # drop 1190 observations that were range estimates for leaf area, plant vegetative height and seed mass, which led to repeats in the data
+
+# Old_Newpriority <- read.csv("OriginalData\\Traits\\TRY\\trait_priority.csv") %>%
+#   rename(TraitID=TRY.trait.ID)
+
+# Traits_Units <- dat3 %>%
+#   select(TraitID, TraitName, CleanTraitName, UnitName) %>%
+#   unique() %>%
+#   mutate(Units=ifelse(CleanTraitName==3121, "g(W)/g(DM)", ifelse(CleanTraitName==3122, "g(W)/g(DM)", UnitName))) %>%
+#   select(-UnitName) %>%
+#   left_join(Old_Newpriority)
+
+# write.csv(Traits_Units, "OriginalData\\Traits\\TRY\\TRYCoRREMerge\\ContTraitUnits.csv", row.names = F)
+
+
+#### Removing observations that do not meet our criteria ####
+
+# Removing dead plants
+health <- dat %>%
+  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
+  filter(DataID==1961) %>%
+  mutate(drop=ifelse(OrigValueStr=="Dead", 1, 0)) %>%
+  select(ObsDataID, drop) %>%
+  unique() #list of dead plants
+
+healthy <- dat3 %>% #merge to drop observations on dead plants
+  left_join(health) %>% 
+  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
+  filter(drop!=1) %>%  #no overlap in dataset
+  select(-drop)
+
+# Removing trees that are not seedlings -- based on data identified specifically as either mature or seedling
+corre_treesp <- read.csv("CompiledData/Species_lists/species_families_trees_2021.csv") %>%
+  mutate(AccSpeciesName=species_matched) #read in which species are trees
+
+gex_treesp <- read.csv("OriginalData/Traits/GEx_species_tree_complete.csv") %>% 
+  filter(tree.non.tree %in% c("tree", "non-tree")) %>% 
+  select(species_matched, family, tree.non.tree) %>% 
+  separate(species_matched, into = c("genus", "species", "other"), sep=" ") %>% 
+  filter(species!="NA") %>% 
+  mutate(species_matched=paste(genus, species, sep=" ")) %>% 
+  select(species_matched, family, tree.non.tree) %>% 
+  unique() %>% 
+  left_join(gexlist) %>% 
+  unique()
+
+treesp <- corre_treesp %>% 
+  bind_rows(gex_treesp) %>% 
+  unique()
+
+tree <- dat %>% #get list of tree observations that were made on seedlings
+  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
+  filter(DataID==413) %>%
+  right_join(treesp) %>%
+  filter(tree.non.tree=="tree") %>%
+  mutate(drop=ifelse(OrigValueStr=="seedlings"|OrigUnitStr==0|OrigValueStr=="seedling"|OrigValueStr=="Seedling (0 - 1 y)"|OrigValueStr=="seedlings, 1st year",  0, 1)) %>%
+  select(ObsDataID, drop) %>%
+  unique()
+
+nontree <- dat3 %>% #merge to drop tree observations that are not seedlings
+  left_join(tree) %>% 
+  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
+  filter(drop!=1) %>%  #no overlap in dataset
+  select(-drop)
+
+# Removing plants that were not measured in natural conditions
+setting <- dat %>% #get list of observations that were not in natural settings
+  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
+  filter(DataID==327) %>%
+  mutate(drop=ifelse(OrigValueStr %in% c("Canadian High Arctic Research Station", "Control Plot", "field","Field", "Field (CG)", "Field (NE)", "field experiment", "Field Experiment", "Field plants", "forest stand", "Forest trees", "Forest understorey", "Fully open overstory 90 days, seedling", "Fully open overstory, seedling","Fully sunlit - Natural environment","High desert", "in situ", "In situ", "La Selva Biological Station", "meadows (M) and pastures (P) on south east to south west exposed slopes", "Montane meadow", "Mosses in forest", "nat env", "natural", "Natural", "natural-environment", "natural env", "natural envireonment", "natural enviroment", "Natural Enviroment", "natural environment", "Natural environment", "Natural Environment", "natural environment, high regional N and S deposition","natural environment, no warming, preccipitation ambient", "natural environment, sun exposed", "Natural Envrionment", "Natural Forest", "natural forest environment", "natural vegetation", "Natural Vegetation", "Natural Vegetation", "natural vegetation, but not top canopy", "natural wetland environment", "natural wetlands (field conditions)", "Natural/C", "natural_environment", "none", "None", "North facing slope", "Shade - Natural environment","South facing slope", "Trees in field"), 0, 1)) %>%
+  select(ObsDataID, drop) %>%
+  unique()
+
+natural <- dat3 %>% # merge to drop observations in non-natural settings
+  left_join(setting) %>% 
+  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
+  filter(drop!=1) %>%  #no overlap in dataset
+  select(-drop)
+
+# Drop traits for trees -- based on whether or not the species is a tree (for all that were not designated as seedling, above)
+splist <- key %>% 
+  select(species_matched) %>%
+  unique() %>%
+  left_join(treesp) %>%
+  na.omit() %>% # drop non fully id'd to species
+  mutate(family2=ifelse(species_matched=="Comandra umbellata", "Santalaceae", 
+                 ifelse(species_matched=="Dianella longifolia", "Asphodelaceae",
+                 ifelse(species_matched=="Lancea tibetica", "Mazaceae", family)))) %>% 
+  select(-family) %>% 
+  rename(family=family2) %>% 
+  unique
+
+# splist_famconflict<-splist %>% 
+#   group_by(species_matched) %>% 
+#   summarize(n=length(family))
+
+cont_traits <- dat3 %>%
+  left_join(splist, by="species_matched") %>%
+  mutate(remove=ifelse(tree.non.tree=="non-tree", 0, 1)) %>%
+  filter(remove==0) %>%
+  select(-tree.non.tree, -AccSpeciesName.y, -remove) %>%
+  rename(AccSpeciesName=AccSpeciesName.x) # drops 54,726 observations
+
+
+#### Add taxonomic information for each species ####
+cont_traits2 <- cont_traits %>%
+  select(DatasetID, ObservationID, family, species_matched, CleanTraitName, StdValue, ErrorRisk, Reference, UnitName, OriglName, TraitID) %>%
+  separate(remove = F, species_matched, into = c("genus", "species"), sep=" ") %>%
+  select(-species)
+
+
+#### Removing trait outliers based on TRY's Error Risk designation ####
+cont_traits3a <- cont_traits2 %>%
+  select(DatasetID, ObservationID, family, genus, species_matched, CleanTraitName, StdValue, ErrorRisk, Reference, UnitName, OriglName, TraitID) %>%
+  mutate(ErrorRisk2=ifelse(is.na(ErrorRisk), 0, ErrorRisk)) %>%
+  filter(ErrorRisk2<3) %>% #removes all observations that are greater than 3 sd from full database mean: drops 28,794 observations
+  select(-ErrorRisk, -ErrorRisk2) %>% 
+  filter(StdValue>0) #removing negative and 0 values (drops 26 observations)
+
+
+#### Removing problem data where there are replicates for ObservationID ####
+cont_traits3 <- cont_traits3a %>% 
+  mutate(remove=ifelse(TraitID==3116 & UnitName=='', 1, 
                 ifelse(TraitID==3109 & OriglName=="Area (dry) cm2", 1, 
                 ifelse(TraitID==3109 & OriglName=="Dry.area.cm2", 1,
                 ifelse(TraitID==3117 & OriglName=="LMA", 1,
@@ -122,144 +256,15 @@ dat3 <- dat2 %>%
                 ifelse(TraitID==3116 & OriglName=="SLA_min", 1, 
                 ifelse(TraitID==3116 & OriglName=="SLA_max", 1, 
                 ifelse(TraitID==1080 & OriglName=="SRL roots >2mm diam (cm/g)", 1,
-                ifelse(TraitID==26 & OriglName=="SeedMassMax" | OriglName=="SeedMassMin",1, 0)))))))))))))))))))))))))))))))))))) %>% # remove problem data where there are replicates for ObservationID
+                ifelse(TraitID==26 & OriglName=="SeedMassMax" | OriglName=="SeedMassMin",1, 0)))))))))))))))))))))))))))))))))))) %>% 
   filter(remove==0) %>%
-  select(-remove) %>%
-  mutate(CleanTraitName=ifelse(TraitID==14, 'leaf_N',
-                        ifelse(TraitID==26, 'seed_dry_mass', 
-                        ifelse(TraitID==47, 'LDMC', 
-                        ifelse(TraitID==55, 'leaf_dry_mass', 
-                        ifelse(TraitID==1080, 'SRL',
-                        ifelse(TraitID==3106, 'plant_height_vegetative', 
-                        ifelse(TraitID==3116, 'SLA', 
-                        ifelse(TraitID==3110, 'leaf_area',
-                        TraitID))))))))) %>%
-  filter(!is.na(StdValue)) %>% 
-  filter(is.na(OrigObsDataID)) %>% # this drops known repeats in TRY
-  filter(UncertaintyName!="Range"&UncertaintyName!="Class range")# this drops 1190 observations that were range estimates for leaf area, plant vegtative height and seed mass. Led to repeats in the data
-
-# Old_Newpriority <- read.csv("OriginalData\\Traits\\TRY\\trait_priority.csv") %>%
-#   rename(TraitID=TRY.trait.ID)
-
-# Traits_Units <- dat3 %>%
-#   select(TraitID, TraitName, CleanTraitName, UnitName) %>%
-#   unique() %>%
-#   mutate(Units=ifelse(CleanTraitName==3121, "g(W)/g(DM)", ifelse(CleanTraitName==3122, "g(W)/g(DM)", UnitName))) %>%
-#   select(-UnitName) %>%
-#   left_join(Old_Newpriority)
-
-# write.csv(Traits_Units, "OriginalData\\Traits\\TRY\\TRYCoRREMerge\\ContTraitUnits.csv", row.names = F)
-
-#### Removing observations that do not meet our criteria ####
-
-# Removing dead plants
-health <- dat %>%
-  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
-  filter(DataID==1961) %>%
-  mutate(drop=ifelse(OrigValueStr=="Dead", 1, 0)) %>%
-  select(ObsDataID, drop) %>%
-  unique() #list of dead plants
-
-healthy <- dat3 %>% #merge to drop observations on dead plants
-  left_join(health) %>% 
-  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
-  filter(drop!=1) %>%  #no overlap in dataset
-  select(-drop)
-
-# Removing trees that are not seedlings -- based on data identified specifically as either mature or seedling
-corre_treesp <- read.csv("CompiledData/Species_lists/species_families_trees_2021.csv") %>%
-  mutate(AccSpeciesName=species_matched) #read in which species are trees
-
-gex_treesp<-read.csv("OriginalData/Traits/GEx_species_tree_complete.csv") %>% 
-  filter(tree.non.tree %in% c("tree", "non-tree")) %>% 
-  select(species_matched, family, tree.non.tree) %>% 
-  separate(species_matched, into = c("genus", "species", "other"), sep=" ") %>% 
-  filter(species!="NA") %>% 
-  mutate(species_matched=paste(genus, species, sep=" ")) %>% 
-  select(species_matched, family, tree.non.tree) %>% 
-  unique() %>% 
-  left_join(gexlist) %>% 
-  unique()
-
-treesp<-corre_treesp %>% 
-  bind_rows(gex_treesp) %>% 
-  unique()
-
-tree <- dat %>% #get list of tree observations that were made on seedlings
-  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
-  filter(DataID==413) %>%
-  right_join(treesp) %>%
-  filter(tree.non.tree=="tree") %>%
-  mutate(drop=ifelse(OrigValueStr=="seedlings"|OrigUnitStr==0|OrigValueStr=="seedling"|OrigValueStr=="Seedling (0 - 1 y)"|OrigValueStr=="seedlings, 1st year",  0, 1)) %>%
-  select(ObsDataID, drop) %>%
-  unique()
-
-nontree <- dat3 %>% #merge to drop tree observations that are not seedlings
-  left_join(tree) %>% 
-  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
-  filter(drop!=1) %>%  #no overlap in dataset
-  select(-drop)
-
-# Removing plants that were not measured in natural conditions
-setting <- dat %>% #get list of observations that were not in natural settings
-  select(DatasetID, DataID, ObsDataID, AccSpeciesID, AccSpeciesName, TraitID, OriglName, TraitName, OrigValueStr, OrigUnitStr, StdValue, UnitName, ErrorRisk) %>%
-  filter(DataID==327) %>%
-  mutate(drop=ifelse(OrigValueStr %in% c("Canadian High Arctic Research Station", "Control Plot", "field","Field", "Field (CG)", "Field (NE)", "field experiment", "Field Experiment", "Field plants", "forest stand", "Forest trees", "Forest understorey", "Fully open overstory 90 days, seedling", "Fully open overstory, seedling","Fully sunlit - Natural environment","High desert", "in situ", "In situ", "La Selva Biological Station", "meadows (M) and pastures (P) on south east to south west exposed slopes", "Montane meadow", "Mosses in forest", "nat env", "natural", "Natural", "natural-environment", "natural env", "natural envireonment", "natural enviroment", "Natural Enviroment", "natural environment", "Natural environment", "Natural Environment", "natural environment, high regional N and S deposition","natural environment, no warming, preccipitation ambient", "natural environment, sun exposed", "Natural Envrionment", "Natural Forest", "natural forest environment", "natural vegetation", "Natural Vegetation", "Natural Vegetation", "natural vegetation, but not top canopy", "natural wetland environment", "natural wetlands (field conditions)", "Natural/C", "natural_environment", "none", "None", "North facing slope", "Shade - Natural environment","South facing slope", "Trees in field"), 0, 1)) %>%
-  select(ObsDataID, drop) %>%
-  unique()
-
-natural <- dat3 %>% #merge to drop observations in non-natural settings
-  left_join(setting) %>% 
-  mutate(drop=ifelse(is.na(drop), 0, drop)) %>% 
-  filter(drop!=1) %>%  #no overlap in dataset
-  select(-drop)
-
-# Drop traits for trees -- based on whether or not the species is a tree (for all that were not designated as seedling, above)
-splist <- key %>% 
-  select(species_matched) %>%
-  unique() %>%
-  left_join(treesp) %>%
-  na.omit() %>% #drop non fully id'd to species
-  mutate(family2=ifelse(species_matched=="Comandra umbellata", "Santalaceae", 
-                 ifelse(species_matched=="Dianella longifolia", "Asphodelaceae",
-                 ifelse(species_matched=="Lancea tibetica", "Mazaceae", family)))) %>% 
-  select(-family) %>% 
-  rename(family=family2) %>% 
-  unique
-
-# splist_famconflict<-splist %>% 
-#   group_by(species_matched) %>% 
-#   summarize(n=length(family))
-
-cont_traits <- dat3 %>%
-  left_join(splist, by="species_matched") %>%
-  mutate(remove=ifelse(tree.non.tree=="non-tree", 0, 1)) %>%
-  filter(remove==0) %>%
-  select(-tree.non.tree, -AccSpeciesName.y, -remove) %>%
-  rename(AccSpeciesName=AccSpeciesName.x) # drops 54,077 observations
-
-
-#### Add taxonomic information for each species ####
-cont_traits2 <- cont_traits %>%
-  select(DatasetID, ObservationID, family, species_matched, CleanTraitName, StdValue, ErrorRisk, Reference) %>%
-  separate(remove = F, species_matched, into = c("genus", "species"), sep=" ") %>%
-  select(-species)
-
-
-#### Removing trait outliers based on TRY's Error Risk designation ####
-cont_traits3 <- cont_traits2 %>%
-  select(DatasetID, ObservationID, family, genus, species_matched, CleanTraitName, StdValue, ErrorRisk, Reference) %>%
-  mutate(ErrorRisk2=ifelse(is.na(ErrorRisk), 0, ErrorRisk)) %>%
-  filter(ErrorRisk2<3) %>% #removes all observations that are greater than 3 sd from full database mean: drops 28,806 observations
-  select(-ErrorRisk, -ErrorRisk2) %>% 
-  filter(StdValue>0) #removing negative and 0 values (drops 22 observations)
+  select(-remove)
 
 
 #### Problem Datasets -- duplicate entries ####
 
 # Problem: We investigated many repeats of similar plant vegetative height, LDMC, and water content values. We think this data was probably methodologically linked in some way to give these similar values (e.g., poor resolution of measurements). 
 # Solution: We think the values are real (despite the methodological linkage), not duplicated data. Therefore, we are keeping all of this data.
-
 
 # Problem: Three observations per plant (same ObservationID). Probably three leaves per plant, but no way to link leaves. Solution: Average.
 d453 <- cont_traits3 %>%
@@ -310,13 +315,12 @@ d1_Ques<-d1.1 %>%
   filter(Reference=='Quested, H. M., J. H. C. Cornelissen, M. C. Press, T. V. Callaghan, R. Aerts, F. Trosien, P. Riemann, D. Gwynn-Jones, A. Kondratchuk, and S. E. Jonasson. 2003. Decomposition of sub-arctic plants with differing nitrogen economies: A functional role for hemiparasites. Ecology 84:3209-3221.')
 
 #Join these by all species and values to see the overlap
-d1<-d1_Ques %>% 
+d1 <- d1_Ques %>% 
   full_join(d1_corn04, by=c('DatasetID', "family", "genus" , "species_matched","CleanTraitName" , "StdValue" )) %>% 
   mutate(ObservationID=ifelse(is.na(ObservationID.y), ObservationID.x, ifelse(is.na(ObservationID.x),ObservationID.y, ObservationID.x))) %>% 
   mutate(Reference=ifelse(is.na(Reference.y), Reference.x, ifelse(is.na(Reference.x),Reference.y, Reference.x))) %>% 
   select(DatasetID, ObservationID, family, genus, species_matched, CleanTraitName, StdValue, Reference) %>% 
   bind_rows(d1_corn96)
-
 
 # Problem: has repeated individuals within the dataset. 
 # Solution: Take the average for each individual for each trait. For each species, find if there is repeated data for all traits collected on an individual.
@@ -325,12 +329,6 @@ d412 <- cont_traits3 %>%
   group_by(DatasetID, ObservationID, family, genus, species_matched, CleanTraitName, Reference) %>% 
   summarize(StdValue=mean(StdValue)) %>% 
   ungroup()
-
-# Problem: Many repeated values for trait 3117 (SLA undefined petiole), but they are attached to unique observations for other traits. Perhaps this is another methodological issue with how SLA was measured. 
-# Solution: Just keep what is there despite the repeats.
-# d400 <- cont_traits3 %>% 
-#   filter(DatasetID==400) %>% 
-#   pivot_wider(names_from=CleanTraitName, values_from=StdValue)
 
 # Problem: Trait 614 (measure of SRL) three values for a few observations. Solution: just average across traits
 d339 <- cont_traits3 %>% 
